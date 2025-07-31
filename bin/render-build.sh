566d2595a63a3321d7bdc9ace2b33beb211f9b53
Christopher Johnston <chris@fuzzylizard.com>
2025-07-31 14:41:00 -0400

    Builds react files when deploying to render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,10 +4,11 @@
 set -o errexit
 
 bundle install
+npm run build
 bin/rails assets:precompile
 bin/rails assets:clean
 
 # If you have a paid instance type, we recommend moving
 # database migrations like this one from the build command
 # to the pre-deploy command:
-bin/rails db:migrate
+# bin/rails db:migrate
